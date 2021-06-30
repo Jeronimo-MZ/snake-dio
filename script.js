36966ce97c6c9dcbac7316f16d81751045e9a3cc
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-01 01:09:40 +0200

    add food

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
     y: 8 * box,
 };
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+};
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -21,32 +26,38 @@ function criarCobrinha() {
     }
 }
 
-document.addEventListener("keydown", (event) => {
-    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
 
-    if (event.keyCode === 37 && direction !== "right") {
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft" && direction !== "right") {
         direction = "left";
     }
 
-    if (event.keyCode === 38 && direction !== "down") {
+    if (event.key === "ArrowUp" && direction !== "down") {
         direction = "up";
     }
 
-    if (event.keyCode === 39 && direction !== "left") {
+    if (event.key === "ArrowRight" && direction !== "left") {
         direction = "right";
     }
 
-    if (event.keyCode === 40 && direction !== "up") {
+    if (event.key === "ArrowDown" && direction !== "up") {
         direction = "down";
     }
 });
 
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
