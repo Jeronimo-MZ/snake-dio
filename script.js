1a73d90dd7328229b3802d1395a56d1a58e2999c
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-01 00:33:27 +0200

    add snake

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const box = 32;
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box,
+};
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, 16 * box, 16 * box);
+    }
 }
 
 criarBG();
